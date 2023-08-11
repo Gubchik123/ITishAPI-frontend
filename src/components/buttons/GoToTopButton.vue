78794e809aa5e2d3ff7a03fa6e62872f1691867f
Nikita <nikita.gubarev31@gmail.com>
2023-08-11 15:22:11 +0300

    Create src/components/buttons/GoToTopButton.vue

---
+++ src/components/buttons/GoToTopButton.vue
@@ -0,0 +1,54 @@
+<template>
+	<button ref="go_to_top_btn" name="Go to top" id="go-to-top-btn">
+		<ion-icon v-pre name="arrow-up-outline"></ion-icon>
+	</button>
+</template>
+
+<script>
+export default {
+	name: "GoToTopButton",
+	mounted() {
+		window.addEventListener("scroll", this.on_scroll);
+		this.$refs.go_to_top_btn.addEventListener("click", function () {
+			document.body.scrollTop = 0; // For Safari
+			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and
+		});
+	},
+	beforeUnmount() {
+		window.removeEventListener("scroll", this.on_scroll);
+	},
+	methods: {
+		on_scroll() {
+			if (
+				document.body.scrollTop > 200 ||
+				document.documentElement.scrollTop > 200
+			)
+				this.$refs.go_to_top_btn.classList =
+					"rounded-circle d-flex justify-content-center align-items-center";
+			else this.$refs.go_to_top_btn.classList = "";
+		},
+	},
+};
+</script>
+
+<style scoped>
+#go-to-top-btn {
+	z-index: 80;
+	border: none;
+	opacity: 0.8;
+	outline: none;
+	position: fixed;
+	transition: 0.2s;
+	width: 50px;
+	height: 50px;
+	right: 30px;
+	bottom: 30px;
+	display: none; /* Hidden by default */
+
+	color: var(--my-text-color);
+	background-color: var(--my-form-bg-color);
+}
+#go-to-top-btn:hover {
+	opacity: 1;
+}
+</style>
