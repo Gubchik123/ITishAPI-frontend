fa2214a87fd9d7e905f731d03a2c49d8ccf91773
Nikita <nikita.gubarev31@gmail.com>
2023-08-11 15:24:24 +0300

    Add components into the App.vue
    Header, RouterView, GoToTopButton, Footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<template></template>
+<template>
+    <div class="wrapper d-flex flex-column min-vh-100">
+        <Header />
+        <router-view />
+        <go-to-top-button />
+        <Footer />
+    </div>
+</template>
 
 <script>
+import Header from "./components/Header.vue";
+import GoToTopButton from "./components/buttons/GoToTopButton.vue";
+import Footer from "./components/Footer.vue";
+
 export default {
 	name: "App",
+    components: { Header, GoToTopButton, Footer }
 };
 </script>
