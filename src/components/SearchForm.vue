48a4e817347b0fbb2da4f8bf2f508a2af107b7fb
Nikita <nikita.gubarev31@gmail.com>
2023-08-11 15:22:46 +0300

    Create src/components/SearchForm.vue

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,15 @@
+<template>
+	<form method="get" action="/blog" role="search" class="d-flex align-items-center">
+		<input
+			name="q"
+			type="search"
+			:value="$route.params.q || ''"
+			aria-label="Title, content..."
+			placeholder="Title, content..."
+			class="form-control search me-2 ms-lg-3 ms-sm-0"
+		/>
+		<button type="submit" class="btn btn-outline-success mt-1">
+			Search
+		</button>
+	</form>
+</template>
