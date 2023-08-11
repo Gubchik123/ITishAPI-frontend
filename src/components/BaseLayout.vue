155a5bb97e7930927b89d1b12cdcd95759e624c4
Nikita <nikita.gubarev31@gmail.com>
2023-08-11 15:25:21 +0300

    Create src/components/BaseLayout.vue
    with general <main> and two Vue slots

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,27 @@
+<template>
+	<main class="my-4">
+		<div class="container">
+			<div class="row">
+				<div class="d-flex justify-content-between align-items-center">
+					<h1 class="mb-4">
+						{{ content_title }}
+					</h1>
+
+                    <slot name="near_content_title"></slot>
+				</div>
+			</div>
+			<div class="row content_block">
+				<slot></slot>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+    name: "MainContent",
+    props: {
+        content_title: { type: String, required: true }
+    }
+};
+</script>
