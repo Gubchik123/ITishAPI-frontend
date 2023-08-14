48c87899352265e761f5b0ea69176331fdda1a9c
Nikita <nikita.gubarev31@gmail.com>
2023-08-14 10:32:48 +0300

    Add url for the ErrorView
    Add url for all other non existent path with redirecting to the ErrorView

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import FAQsView from "../views/FaqsView.vue";
 import FeedbackView from "../views/FeedbackView.vue";
+import ErrorView from "../views/ErrorView.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,15 +14,24 @@ const router = createRouter({
 			alias: "/home",
 			component: HomeView,
 		},
-        {
-            path: "/faqs",
-            name: "faqs",
-            component: FAQsView,
-        },
+		{
+			path: "/faqs",
+			name: "faqs",
+			component: FAQsView,
+		},
 		{
 			path: "/feedback",
 			name: "feedback",
 			component: FeedbackView,
+		},
+		{
+			path: "/error",
+			name: "error",
+			component: ErrorView,
+		},
+		{
+			path: "/:pathMatch(.*)*",
+			redirect: "/error",
 		},
 	],
 });
