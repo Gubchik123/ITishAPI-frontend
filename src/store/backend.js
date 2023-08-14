fbe59c5cdefe70496528c1805273b5fa3ba8af22
Nikita <nikita.gubarev31@gmail.com>
2023-08-14 14:28:32 +0300

    Create store/backend.js
    for storing info about backend, such as server url...

---
+++ src/store/backend.js
@@ -0,0 +1,13 @@
+export default {
+    namespaced: true,
+	state: {
+		backend_url: "http://127.0.0.1:8000/api",
+	},
+	mutations: {},
+	actions: {},
+	getters: {
+		server_url: (state) => {
+			return state.backend_url;
+		},
+	},
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vuex from "vuex";
 // import Vue from "vue";
 
+import backend from "./backend";
 import error from "./error";
 
 // Vue.use(Vuex);
 
 const store = new Vuex.Store({
 	modules: {
+        backend: backend,
 		error: error,
 	},
 });
