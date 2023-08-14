dbaf8837eea1aaefb25edd3f197c5a16a14df14d
Nikita <nikita.gubarev31@gmail.com>
2023-08-14 14:34:55 +0300

    Create views/blog/PostListView.vue

---
+++ src/views/blog/PostListView.vue
@@ -0,0 +1,56 @@
+<template>
+	<BlogLayout :title="$route.query.q ? 'Results' : 'All posts'">
+		<template v-if="posts === null">
+			<post-placeholder :key="index" v-for="index in 5" />
+		</template>
+		<template v-else-if="Array.isArray(posts) && posts.length === 0">
+			<Alert
+				v-if="$route.query.q"
+				category="info"
+				:message="
+					'There are no posts with: `' + $route.query.q + '` yet'
+				"
+			/>
+			<Alert v-else category="primary" message="There are no posts yet" />
+		</template>
+		<template v-else>
+			<Post :key="post.id" v-for="post in posts" :post="post" />
+		</template>
+	</BlogLayout>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+import BlogLayout from "./BlogLayout.vue";
+import Alert from "../../components/Alert.vue";
+import { get_all_posts } from "../../api/blog";
+
+import Post from "./components/Post.vue";
+import PostPlaceholder from "./components/PostPlaceholder.vue";
+
+export default {
+	name: "PostListView",
+	components: { BlogLayout, Post, PostPlaceholder, Alert },
+	data() {
+		return { posts: null };
+	},
+	created() {
+		setTimeout(() => {
+			// To look at post placeholders
+			get_all_posts(this.server_url, this.$route.query.q).then((response) => {
+				this.posts = response;
+			});
+		}, 1000);
+	},
+	computed: {
+		...mapGetters("backend", ["server_url"]),
+	},
+};
+</script>
+
+<style scoped>
+.item_info {
+	color: white;
+}
+</style>
