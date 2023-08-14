618a9b1ac7410fbf00eb0aecc996a313200e9bb5
Nikita <nikita.gubarev31@gmail.com>
2023-08-14 10:31:48 +0300

    Create ErrorView.vue

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,26 @@
+<template>
+	<BaseLayout :title="code" :content_title="name">
+		<div class="alert alert-danger" role="alert">
+			<div class="float_left me-3">
+				<ion-icon v-pre name="warning"></ion-icon>
+			</div>
+			<div class="h-100 d-flex justify-content-center align-items-center">
+				<h5>{{ description }}</h5>
+			</div>
+		</div>
+	</BaseLayout>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+import BaseLayout from "./BaseLayout.vue";
+
+export default {
+	name: "ErrorView",
+	components: { BaseLayout },
+	computed: {
+		...mapGetters("error", ["code", "name", "description"]),
+	},
+};
+</script>
