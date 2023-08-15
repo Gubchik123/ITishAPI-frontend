dd5da14ed4a94384471c68de7c64311f674f11d3
Nikita <nikita.gubarev31@gmail.com>
2023-08-15 14:17:34 +0300

    Add url for the SignupView into the auth_routes.js

--- src/router/auth_routes.js
+++ src/router/auth_routes.js
@@ -1,6 +1,12 @@
+import SignupView from "../views/auth/SignupView.vue";
 import LoginView from "../views/auth/LoginView.vue";
 
 export default [
+    {
+        path: "signup",
+        name: "signup",
+        component: SignupView,
+    },
     {
         path: "login",
         name: "login",
