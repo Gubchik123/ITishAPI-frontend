b98ceaf4d1de9d59b59625be81264193509d50a1
Nikita <nikita.gubarev31@gmail.com>
2023-08-15 15:22:41 +0300

    Create src/mixins/LoginRequiredMixin.js

---
+++ src/mixins/LoginRequiredMixin.js
@@ -0,0 +1,8 @@
+export default {
+    beforeCreate() {
+        if (!localStorage.getItem("access_token")) {
+            // TODO: Redirect to the user's profile page
+            this.$router.push({ name: "login" });
+        }
+    }
+}
