c0c2c70f627223230adec9a199ff1153b54213b7
Nikita <nikita.gubarev31@gmail.com>
2023-08-16 16:49:44 +0300

    Add 'beforeEnter' for the LoginView and SignupView
    to check if user is already authenticated

--- src/router/auth_routes.js
+++ src/router/auth_routes.js
@@ -1,15 +1,23 @@
 import SignupView from "../views/auth/SignupView.vue";
 import LoginView from "../views/auth/LoginView.vue";
 
+function reject_if_user_is_authenticated(to, from, next) {
+    if (localStorage.getItem("access_token")) {
+        next({ name: "me" });
+    } else next();
+}
+
 export default [
-    {
-        path: "signup",
-        name: "signup",
-        component: SignupView,
-    },
-    {
-        path: "login",
-        name: "login",
-        component: LoginView,
-    },
-]
+	{
+		path: "signup",
+		name: "signup",
+		component: SignupView,
+		beforeEnter: [reject_if_user_is_authenticated],
+	},
+	{
+		path: "login",
+		name: "login",
+		component: LoginView,
+		beforeEnter: [reject_if_user_is_authenticated],
+	},
+];
