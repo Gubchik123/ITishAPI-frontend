435dd1f81f61c11d3882b18e2d0a4cbab2de978e
Nikita <nikita.gubarev31@gmail.com>
2023-08-16 16:59:46 +0300

    Create views/blog/components/CommentPlaceholder.vue

---
+++ src/views/blog/components/CommentPlaceholder.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="alert alert-dark placeholder-glow" role="alert">
+		<div class="d-flex justify-content-between">
+			<span class="placeholder col-12 placeholder-lg"></span>
+		</div>
+		<div class="item_info d-flex justify-content-between">
+			<div class="item_author me-3">
+				<span class="placeholder col-6 placeholder-sm"></span>
+			</div>
+			<div class="item_created">
+                <b>Created:</b> &nbsp; 
+                <span class="placeholder col-12 placeholder-sm"></span>
+            </div>
+		</div>
+	</div>
+</template>
