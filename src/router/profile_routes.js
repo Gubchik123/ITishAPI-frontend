f204b0eff969e9817be8a2817ee574b5ac3b2f99
Nikita <nikita.gubarev31@gmail.com>
2023-08-16 16:59:09 +0300

    Add urls for the UserPostsView

--- src/router/profile_routes.js
+++ src/router/profile_routes.js
@@ -1,5 +1,6 @@
 import MeDetailView from "../views/profile/MeDetailView.vue";
 import UserDetailView from "../views/profile/UserDetailView.vue";
+import UserPostsView from "../views/profile/UserPostsView.vue";
 
 export default [
 	{
@@ -9,8 +10,19 @@ export default [
         meta: { requires_auth: true }
 	},
 	{
+		path: "me/posts",
+		name: "my-posts",
+		component: UserPostsView,
+        meta: { requires_auth: true }
+	},
+	{
 		path: ":username",
 		name: "user",
 		component: UserDetailView,
+	},
+	{
+		path: ":username/posts",
+		name: "user-posts",
+		component: UserPostsView,
 	},
 ];
